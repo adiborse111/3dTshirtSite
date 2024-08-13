3dff3d5517a8cdd374d3ad19f47baa2cc7f435bf
adiborse111 <adi.borse111@gmail.com>
2024-08-13 22:11:18 +0530

    final commit

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,6 +1,6 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import { Configuration, OpenAIApi } from "openai";
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi} from 'openai';
 
 dotenv.config();
 
@@ -12,28 +12,28 @@ const config = new Configuration({
 
 const openai = new OpenAIApi(config);
 
-router.route("/").get((req, res) => {
-  res.status(200).json({ message: "hello from dalle routes" });
-});
+router.route('/').get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" })
+})
 
-router.route("/").post(async (req, res) => {
+router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
     const response = await openai.createImage({
       prompt,
       n: 1,
-      size: "1024x1024",
-      response_format: "b64_json",
+      size: '1024x1024',
+      response_format: 'b64_json'
     });
 
     const image = response.data.data[0].b64_json;
 
     res.status(200).json({ photo: image });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "something went wrong" });
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" })
   }
-});
+})
 
 export default router;
--- src/components/AIPicker.jsx
+++ src/components/AIPicker.jsx
@@ -1,8 +1,41 @@
+import CustomButton from "./CustomButton";
 
-const AIPicker = () => {
+const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
   return (
-    <div>AIPicker</div>
-  )
-}
+    <div className="aipicker-container">
+      <textarea
+        className="aipicker-textarea"
+        placeholder="Ask AI..."
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+      <div className="flex flex-wrap gap-3">
+        {generatingImg ? (
+          <CustomButton
+            type="outline"
+            title="Asking AI..."
+            customStyles="text-xs"
+          />
+        ) : (
+          <>
+            <CustomButton
+              type="outline"
+              title="AI logo"
+              handleclick={() => handleSubmit ("logo")}
+              customStyles="text-xs"
+            />
+            <CustomButton
+              type="filled"
+              title="AI Full"
+              handleclick={() => handleSubmit ("full")}
+              customStyles="text-xs"
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default AIPicker
+export default AIPicker;
